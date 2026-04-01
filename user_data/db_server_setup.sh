a071e6609b49a4747dfc842412d1dccf01b7aacc
unknown <jamesadewara1@gmail.com>
2026-04-01 15:42:03 +0100

    still on it, db aws linux issue

--- user_data/db_server_setup.sh
+++ user_data/db_server_setup.sh
@@ -2,13 +2,16 @@
 # Update the ec2 system
 sudo yum update -y
 
-# Install PostgreSQL 13 (Stable version for Amazon Linux 2)
-sudo amazon-linux-extras enable postgresql13
-sudo yum install -y postgresql-server
 
-# Initialize the database cluster
-sudo postgresql-setup initdb
+# Install the software
+sudo dnf install -y postgresql15-server
 
-# Start and Enable PostgreSQL service
+# Initialize the database (Mandatory on first install)
+sudo postgresql-setup --initdb
+
+# Start the engine
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
+
+# Try the connection again
+sudo -u postgres psql
